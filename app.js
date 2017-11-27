573c49f6224a0afcb7e1ffcf7530881848ab4fa0
amad.incubasys <amad.zahid@mail.incubasys.com.pk>
2017-11-27 16:15:45 +0500

    todo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var _ = require('underscore');
+var bodyParser = require('body-parser');
 var todos = [];
 var todoNext = 1;
 
@@ -18,14 +19,14 @@ app.get('/todos', function(req , res){
 
 app.get('/todo/:id', function(req , res){
     var todoId = parseInt(req.params.id, 10);
-    // var todoId = req.params.id;
-    var matchTodo;
+    var matchTodo = _.findWhere(todos, {id: todoId});
 
-    todos.forEach(function(todo){
-        if(todoId === todo.id){
-            matchTodo = todo;
-        }
-    });
+    // var matchTodo;
+    // todos.forEach(function(todo){
+    //     if(todoId === todo.id){
+    //         matchTodo = todo;
+    //     }
+    // });
 
     if(matchTodo){
         res.json(matchTodo);
@@ -35,8 +36,13 @@ app.get('/todo/:id', function(req , res){
 });
 
 app.post('/todos', function(req , res){
-    var body = req.body;
+    var body = _.pick(req.body , 'description' , 'completed');
 
+    if(! _.isBoolean(body.completed) || ! _.toString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
     body.id = todoNext++;
     todos.push(body);
     res.json(body);
